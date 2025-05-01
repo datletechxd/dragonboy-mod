cb110d39a80de2935f7ee6046ec67afef3954b24
datletechxd <lhthanhdat1912@gmail.com>
2025-05-01 18:55:52 +0700

    feat(mod): add DisplayGameInfo for mod info

---
+++ GameAssembly/Mod/DisplayGameInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameAssembly.Mod
+{
+	internal class DisplayGameInfo
+	{
+		public static DisplayGameInfo _Instance;
+
+		public static DisplayGameInfo getInstance()
+		{
+			if (_Instance == null)
+			{
+				_Instance = new DisplayGameInfo();
+			}
+			return _Instance;
+		}
+	}
+}
