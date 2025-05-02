762f7e808332940a60bc155cc7c924e6465e96b1
Le Hoang Thanh Dat <lhthanhdat1912@gmail.com>
2025-05-02 15:19:39 +0700

    feat(mod): auto-use buff to current character

--- GameAssembly/Mod/AutoSkill.cs
+++ GameAssembly/Mod/AutoSkill.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace GameAssembly.Mod
 {
 	internal class AutoSkill
 	{
 		public static AutoSkill _Instance;
+
+		public static bool isAutoBuff;
+
+		public static sbyte ID_SKILL_BUFF = 7;
 
 		public static AutoSkill getInstance()
 		{
@@ -23,6 +28,36 @@ namespace GameAssembly.Mod
 			MyVector myVector = new MyVector();
 			myVector.addElement(global::Char.myCharz());
 			return myVector;
+		}
+
+		public static bool isSkillAvailable(out Skill skill)
+		{
+			skill = global::Char.myCharz().getSkill(new SkillTemplate { id = ID_SKILL_BUFF });
+			if (skill == null)
+			{
+				GameScr.info1.addInfo("Không tìm thấy kỹ năng trị thương.", 0);
+				return false;
+			}
+			return true;
+		}
+
+		public static void autoUseSkillBuff()
+		{
+			while (isAutoBuff)
+			{
+				Skill skill;
+				if (isSkillAvailable(out skill))
+				{
+					if (mSystem.currentTimeMillis() - skill.lastTimeUseThisSkill > skill.coolDown)
+					{
+						Service.gI().selectSkill((int)ID_SKILL_BUFF);
+						Service.gI().sendPlayerAttack(new MyVector(), getMyVector(), -1);
+						Service.gI().selectSkill((int)global::Char.myCharz().myskill.template.id);
+						skill.lastTimeUseThisSkill = mSystem.currentTimeMillis();
+					}
+				}
+				Thread.Sleep(skill.coolDown);
+			}
 		}
 	}
 }
