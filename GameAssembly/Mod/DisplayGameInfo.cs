3110cd5d54b3886c57d721bd7de6fe8b64a5a535
Le Hoang Thanh Dat <lhthanhdat1912@gmail.com>
2025-05-02 10:03:22 +0700

    refactor(mod): call paintModInfo after extract

--- GameAssembly/Mod/DisplayGameInfo.cs
+++ GameAssembly/Mod/DisplayGameInfo.cs
@@ -20,15 +20,7 @@ namespace GameAssembly.Mod
 
 		public static void paint(mGraphics g)
 		{
-			mFont.tahoma_7b_white.drawString(g, string.Concat(new string[]
-			{
-				"Map: ",
-				TileMap.mapNames[TileMap.mapID],
-				" [",
-				TileMap.zoneID.ToString(),
-				"]"
-			}), 25, GameCanvas.h - 200, 0);
-			mFont.tahoma_7b_white.drawString(g, "Time: " + DateTime.Now.ToString("HH:mm:ss dd/MM/yyyy"), 25, GameCanvas.h - 210, 0);
+			paintModInfo(g);
 		}
 
 		public static void paintModInfo(mGraphics g)
