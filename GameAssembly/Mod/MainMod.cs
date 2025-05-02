7d0755c8523f1d8e376311f5ad1366afbc29e1b2
Le Hoang Thanh Dat <lhthanhdat1912@gmail.com>
2025-05-02 09:29:52 +0700

    feat(mod): get list of characters in map

--- GameAssembly/Mod/MainMod.cs
+++ GameAssembly/Mod/MainMod.cs
@@ -9,6 +9,8 @@ namespace GameAssembly.Mod
 	{
 		public static MainMod _Instance;
 
+		public static List<global::Char> listCharsInMap = new List<global::Char>();
+
 		public static int runSpeed = 8;
 
 		public static MainMod getInstance()
@@ -22,7 +24,21 @@ namespace GameAssembly.Mod
 
 		public static void update()
 		{
+			getListCharsInMap();
 			global::Char.myCharz().cspeed = runSpeed;
+		}
+
+		private static void getListCharsInMap()
+		{
+			listCharsInMap.Clear();
+			for (int i = 0; i < GameScr.vCharInMap.size(); i++)
+			{
+				global::Char @char = (global::Char)GameScr.vCharInMap.elementAt(i);
+				if (@char.cName != null && @char.cName != "" && !@char.isPet && !@char.isMiniPet && !@char.cName.StartsWith("#") && !@char.cName.StartsWith("$") && @char.cName != "Trọng tài")
+				{
+					listCharsInMap.Add(@char);
+				}
+			}
 		}
 	}
 }
