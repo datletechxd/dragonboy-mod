0c9429b7b91f3f3bc5adf29858ab134449c67ef8
Le Hoang Thanh Dat <lhthanhdat1912@gmail.com>
2025-05-02 10:00:41 +0700

    refactor(mod): extract map & time to paintModInfo

--- GameAssembly/Mod/DisplayGameInfo.cs
+++ GameAssembly/Mod/DisplayGameInfo.cs
@@ -31,6 +31,19 @@ namespace GameAssembly.Mod
 			mFont.tahoma_7b_white.drawString(g, "Time: " + DateTime.Now.ToString("HH:mm:ss dd/MM/yyyy"), 25, GameCanvas.h - 210, 0);
 		}
 
+		public static void paintModInfo(mGraphics g)
+		{
+			mFont.tahoma_7b_white.drawString(g, string.Concat(new string[]
+			{
+				"Map: ",
+				TileMap.mapNames[TileMap.mapID],
+				" [",
+				TileMap.zoneID.ToString(),
+				"]"
+			}), 25, GameCanvas.h - 200, 0);
+			mFont.tahoma_7b_white.drawString(g, "Time: " + DateTime.Now.ToString("HH:mm:ss dd/MM/yyyy"), 25, GameCanvas.h - 210, 0);
+		} 
+
 		public static bool isBoss(global::Char ch)
 		{
 			return ch.cName != null && ch.cName != "" && !ch.isPet && !ch.isMiniPet && char.IsUpper(char.Parse(ch.cName.Substring(0, 1))) && ch.cName != "Trọng tài" && !ch.cName.StartsWith("#") && !ch.cName.StartsWith("$");
