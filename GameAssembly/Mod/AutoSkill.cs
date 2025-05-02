f46ec03020bce1632c23d051a698af17d0b8ec7c
Le Hoang Thanh Dat <lhthanhdat1912@gmail.com>
2025-05-02 14:50:25 +0700

    feat(mod): add AutoSkill to auto-use skills

---
+++ GameAssembly/Mod/AutoSkill.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameAssembly.Mod
+{
+	internal class AutoSkill
+	{
+		public static AutoSkill _Instance;
+
+		public static AutoSkill getInstance()
+		{
+			if (_Instance == null)
+			{
+				_Instance = new AutoSkill();
+			}
+			return _Instance;
+		}
+	}
+}
