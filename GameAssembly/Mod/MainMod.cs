13c4e75c1c5d034cb423499d88a1ffa1f812376b
Le Hoang Thanh Dat <lhthanhdat1912@gmail.com>
2025-05-02 09:24:50 +0700

    style(formatting): convert spaces to tabs

--- GameAssembly/Mod/MainMod.cs
+++ GameAssembly/Mod/MainMod.cs
@@ -5,24 +5,24 @@ using System.Text;
 
 namespace GameAssembly.Mod
 {
-    internal class MainMod
-    {
-        public static MainMod _Instance;
+	internal class MainMod
+	{
+		public static MainMod _Instance;
 
-        public static int runSpeed = 8;
+		public static int runSpeed = 8;
 
-        public static MainMod getInstance()
-        {
-            if (_Instance == null)
-            {
-                _Instance = new MainMod();
-            }
-            return _Instance;
-        }
+		public static MainMod getInstance()
+		{
+			if (_Instance == null)
+			{
+				_Instance = new MainMod();
+			}
+			return _Instance;
+		}
 
-        public static void update()
-        {
-            global::Char.myCharz().cspeed = runSpeed;
-        }
-    }
+		public static void update()
+		{
+			global::Char.myCharz().cspeed = runSpeed;
+		}
+	}
 }
