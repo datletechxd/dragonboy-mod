3c3e9c820f6cec688b7bcf73fbaf886b819aef5e
Le Hoang Thanh Dat <lhthanhdat1912@gmail.com>
2025-05-03 12:52:38 +0700

    feat(mod): add auto pean

---
+++ GameAssembly/Mod/AutoPean.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameAssembly.Mod
+{
+	internal class AutoPean
+	{
+		public static AutoPean _Instance;
+
+		public static AutoPean getInstance()
+		{
+			if (_Instance == null)
+			{
+				_Instance = new AutoPean();
+			}
+			return _Instance;
+		}
+	}
+}
