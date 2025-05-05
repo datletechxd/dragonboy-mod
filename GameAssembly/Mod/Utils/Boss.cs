0054efeda3914b1b273404510bc3414670477f1c
Le Hoang Thanh Dat <lhthanhdat1912@gmail.com>
2025-05-05 22:44:43 +0700

    feat(boss): get map id boss appear

--- GameAssembly/Mod/Utils/Boss.cs
+++ GameAssembly/Mod/Utils/Boss.cs
@@ -7,5 +7,16 @@ namespace GameAssembly.Mod.Utils
 {
 	internal class Boss
 	{
+		public int getMapID(string mapName)
+		{
+			for (int i = 0; i < TileMap.mapNames.Length; i++)
+			{
+				if (TileMap.mapNames[i].Equals(mapName))
+				{
+					return i;
+				}
+			}
+			return -1;
+		}
 	}
 }
