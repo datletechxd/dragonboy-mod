8291d79bb798b953dacde08576e4e3006317bab1
Le Hoang Thanh Dat <lhthanhdat1912@gmail.com>
2025-05-05 22:52:10 +0700

    feat(boss): implement boss tracking & spawn timing

--- GameAssembly/Mod/Utils/Boss.cs
+++ GameAssembly/Mod/Utils/Boss.cs
@@ -7,6 +7,27 @@ namespace GameAssembly.Mod.Utils
 {
 	internal class Boss
 	{
+		public string NameBoss;
+
+		public string MapName;
+
+		public int MapId;
+
+		public DateTime AppearTime;
+
+		public Boss(string chatVip)
+		{
+			chatVip = chatVip.Replace("BOSS ", "").Replace(" vừa xuất hiện tại ", "|").Replace(" appear at ", "|");
+			string[] array = chatVip.Split(new char[]
+			{
+				 '|'
+			});
+			this.NameBoss = array[0].Trim();
+			this.MapName = array[1].Trim();
+			this.MapId = this.getMapID(this.MapName);
+			this.AppearTime = DateTime.Now;
+		}
+
 		public int getMapID(string mapName)
 		{
 			for (int i = 0; i < TileMap.mapNames.Length; i++)
