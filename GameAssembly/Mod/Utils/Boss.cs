cd1c289600f7cd48799e5f8c98e8ab721680664d
Le Hoang Thanh Dat <lhthanhdat1912@gmail.com>
2025-05-06 09:02:29 +0700

    feat(boss): add structure to display time & location

--- GameAssembly/Mod/Utils/Boss.cs
+++ GameAssembly/Mod/Utils/Boss.cs
@@ -39,5 +39,33 @@ namespace GameAssembly.Mod.Utils
 			}
 			return -1;
 		}
+
+		public void paint(mGraphics g, int x, int y, int align)
+		{
+			TimeSpan timeSpan = DateTime.Now.Subtract(this.AppearTime);
+			int num = (int)timeSpan.TotalSeconds;
+			mFont mFont = mFont.tahoma_7_yellow;
+			if (TileMap.mapID == this.MapId)
+			{
+				mFont = mFont.tahoma_7_red;
+				for (int i = 0; i < GameScr.vCharInMap.size(); i++)
+				{
+					if (((global::Char)GameScr.vCharInMap.elementAt(i)).cName.Equals(this.NameBoss))
+					{
+						mFont = mFont.tahoma_7b_red;
+						break;
+					}
+				}
+			}
+			mFont.drawString(g, string.Concat(new string[]
+			{
+				 this.NameBoss,
+				 " - ",
+				 this.MapName,
+				 " - ",
+				 (num < 60) ? (num.ToString() + "s") : (timeSpan.Minutes.ToString() + "ph"),
+				 " trước"
+			}), x, y, align);
+		}
 	}
 }
