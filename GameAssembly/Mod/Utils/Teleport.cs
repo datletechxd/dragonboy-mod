ad9514542ef1913aca8b218d2efb3a2c5b2ddfaa
Le Hoang Thanh Dat <lhthanhdat1912@gmail.com>
2025-05-07 20:08:08 +0700

    feat(mod): teleport feature to character & npc

--- GameAssembly/Mod/Utils/Teleport.cs
+++ GameAssembly/Mod/Utils/Teleport.cs
@@ -13,5 +13,20 @@ namespace GameAssembly.Mod.Utils
 			global::Char.myCharz().cy = y;
 			Service.gI().charMove();
 		}
+
+		public static void teleportToFocus()
+		{
+			if (global::Char.myCharz().charFocus != null)
+			{
+				teleportCoordinates(global::Char.myCharz().charFocus.cx, global::Char.myCharz().charFocus.cy);
+				return;
+			}
+			if (global::Char.myCharz().npcFocus != null)
+			{
+				teleportCoordinates(global::Char.myCharz().npcFocus.cx, global::Char.myCharz().npcFocus.cy - 3);
+				return;
+			}
+			GameScr.info1.addInfo("Không Có Mục Tiêu!", 0);
+		}
 	}
 }
